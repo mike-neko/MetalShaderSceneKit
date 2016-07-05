0a24ea60ef10a3b0c30de06d82a781dc1f328aa4
Makoto Ikeda <mike.app.info@gmail.com>
2016-07-06 00:15:44 +0900

    ライト削除

--- MetalShaderSceneKit/GameViewController.swift
+++ MetalShaderSceneKit/GameViewController.swift
@@ -30,20 +30,6 @@ class GameViewController: UIViewController {
         // place the camera
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
         
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = SCNLightTypeOmni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = SCNLightTypeAmbient
-        ambientLightNode.light!.color = UIColor.darkGrayColor()
-        scene.rootNode.addChildNode(ambientLightNode)
-        
         // retrieve the ship node
         let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
         
